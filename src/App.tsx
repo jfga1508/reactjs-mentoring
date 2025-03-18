da12e7c1ecd692296e7129e370050153158f0c62
jfga1508 <jeanfrancois_gonzalezarango@apple.com>
2025-03-18 18:54:42 -0500

    Added Module 1 tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/counter/Counter";
+import GenreSelector from "./components/genreSelector/GenreSelector";
+import SearchBar from "./components/search/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const genres = ["All", "Documentary", "Comedy", "Horror", "Crime"];
+  const [currentGenre, setCurrentGenre] = useState<string>(genres[0]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Counter initial={0} />
+      <SearchBar
+        initialQuery=""
+        onSearch={(query) => console.log("Callback query: ", query)}
+      />
+      <GenreSelector
+        genres={genres}
+        selectedGenre={currentGenre}
+        onSelect={(genre) => {
+          setCurrentGenre(genre);
+          console.log("Callback genre: ", genre);
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,16 @@
+import { createElement, useState } from "react"
+
+const Counter = ({initial} : {initial: number}) => {
+    const [count, setCount] = useState(initial)
+
+    return createElement(
+        'div',
+        { className: 'card' },
+        createElement('button', {onClick: () => setCount((count) => count - 1)}, '-'),
+        createElement('span', null, count),
+        createElement('button', {onClick: () => setCount((count) => count + 1)}, '+'),
+    )
+  }
+  
+  export default Counter
+  
