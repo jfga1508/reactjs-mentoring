94a9f9151857a27e0608908c0e2c6dac79031c8e
jfga1508 <jeanfrancois_gonzalezarango@apple.com>
2025-03-25 16:12:54 -0500

    fix: Resolved git comments.

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,16 +1,23 @@
-import { createElement, useState } from "react"
+import { createElement, useState } from "react";
 
-const Counter = ({initial} : {initial: number}) => {
-    const [count, setCount] = useState(initial)
+const Counter = ({ initial }: { initial: number }) => {
+  const [count, setCount] = useState(initial);
 
-    return createElement(
-        'div',
-        { className: 'card' },
-        createElement('button', {onClick: () => setCount((count) => count - 1)}, '-'),
-        createElement('span', null, count),
-        createElement('button', {onClick: () => setCount((count) => count + 1)}, '+'),
+  return createElement(
+    "div",
+    { className: "card" },
+    createElement(
+      "button",
+      { onClick: () => setCount((count) => count - 1) },
+      "-"
+    ),
+    createElement("span", null, count),
+    createElement(
+      "button",
+      { onClick: () => setCount((count) => count + 1) },
+      "+"
     )
-  }
-  
-  export default Counter
-  
+  );
+};
+
+export default Counter;
